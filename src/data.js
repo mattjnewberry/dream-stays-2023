b180a081feb00ca0bc8913ba41f7b8d3850ee0e9
Esther <90865704+Esther-Aworinde@users.noreply.github.com>
2022-06-29 15:22:35 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 let stays = [
   {
-    id: "theritz",
-    name: "The Ritz",
+    id: "mondrian",
+    name: "Mondrian London",
     img: "https://media.cntraveler.com/photos/5f6198aa987090832029b181/16:9/w_2560%2Cc_limit/ritz-london-exterior.jpg",
     description:
       "We are London’s most iconic hotel. A five star haven on Piccadilly that is famous the world over for its historic elegance, impeccable service, impressive suites, and legendary Afternoon Tea.",
