33c7e30aa9543309758e5bc5eb32937a89612ef7
Esther <90865704+Esther-Aworinde@users.noreply.github.com>
2022-06-29 15:28:53 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,10 +2,10 @@ let stays = [
   {
     id: "mondrian",
     name: "Mondrian London",
-    img: "https://media.cntraveler.com/photos/5f6198aa987090832029b181/16:9/w_2560%2Cc_limit/ritz-london-exterior.jpg",
+    img: "https://hotelbusiness.com/wp-content/uploads/2020/10/Mondrian-Shoreditch.jpg",
     description:
-      "We are London’s most iconic hotel. A five star haven on Piccadilly that is famous the world over for its historic elegance, impeccable service, impressive suites, and legendary Afternoon Tea.",
-    nearby: ["London eye", "Buckingham Palace", "Tower Bridge"],
+      "Exposed brick and hardwood floors, the trendy rooms come with Wi-Fi, minibars and original artwork, plus marble bathrooms with rainfall and steam showers",
+    nearby: ["London eye", "Buckingham Palace", "Tower of London"],
   },
   {
     id: "theritz",
