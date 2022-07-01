3ba44fc17cbe88234e9aa7f69b5a870c6299e6bc
Matt Newberry <manewberry@expediagroup.com>
2022-07-01 20:20:51 +0100

    Update data.js
    The creativity was fantastic with the fictional reviews and descriptions, but probably best to remove them to avoid any potential slander/defamation since this repo is public :smile: :+1:
    Alternatively, you can make this repo private by going to Settings -> Scrolling down to the bottom -> Change Visibility -> Set it to private and save

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 let stays = [
   {
     id: "mondrian",
-    name: "Mondrian London",
+    name: "Mondrian London (Fictional)",
     img: "https://hotelbusiness.com/wp-content/uploads/2020/10/Mondrian-Shoreditch.jpg",
     description:
       "Exposed brick and hardwood floors, the trendy rooms come with Wi-Fi, minibars and original artwork, plus marble bathrooms with rainfall and steam showers",
@@ -10,14 +10,14 @@ let stays = [
     activity:['Ghost Storytelling'],
     videoUrl:'https://www.youtube.com/watch?v=MhB9Ho-0wxw',
     price:'£145 a night',
-    reviews1:'6 Stars: I absolutely love this hotel. its rustic vibes totes fit my aethetc. it was amazingly gorge',
-    reviews2:'5 Stars: This was A STUPENDOUS hotel, my entire buisness was increased by my vist. However my ex wife found me at this location and made me pay child support.',
+    reviews1:'exmaple review here',
+    reviews2:'exmaple review here',
     services: '42-inch LCD TV,Library ,Dry cleaning service,  Tennis lessons'
  
   },
   {
     id: "lanesborough",
-    name: "The Lanesborough",
+    name: "Fictional The Lanesborough (Fictional)",
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJCvMkR-gvXVjMh0WmfMq-SBLSi4O2EDzqiA&usqp=CAU",
     description:
       "Refined rooms feature free Wi-Fi, marble bathrooms and flat-screens; some have balconies with park views. Suites add living rooms, dressing areas and whirlpool tubs. A lavish 4-bedroom suite includes a chauffeur-driven car service.",
@@ -26,14 +26,14 @@ let stays = [
     activity:['Trainspotting'],
     videoUrl:'https://www.youtube.com/watch?v=2sCTkwOQsCI',
     price:'£670 a night',
-    reviews1: '8 Stars: Everything was perfect.The atmosphere was friendly and welcoming. The restaurant was a delight.',
-    reviews2: '5 Stars: The butler service or lack thereof. We had booked this hotel for a landmark birthday and were very sad that this was not as expected. It was a shame as it marred an otherwise wonderful 3 days!',
+    reviews1: 'exmaple review here',
+    reviews2: 'exmaple review here',
     services: 'Internet, Pool,Computer station    '
   },
   { 
 
     id: "victoria",
-    name: "The Resident Victoria",
+    name: "The Resident Victoria (Fictional)",
     img: "https://www.eyerevolution.co.uk/wp-content/uploads/2009/10/emirates-palace1.jpg",
     description:
       "Sleek rooms feature kitchenettes with Nespresso machines, along with free Wi-Fi, flat-screen TVs and laptop-size safes. All have tea and coffeemaking facilities, and designer toiletries. Upgraded rooms add sofabeds.",
@@ -42,8 +42,8 @@ let stays = [
     activity:['Monarchy Gazing'],
     videoUrl:'https://www.youtube.com/watch?v=2yB0d4PYPqg',
     price:'£300 a night',
-    reviews1:'10 Stars: I want to give 100 stars but I can only give 10, BEST HOTEL EVER!!!!',
-    reviews2:'6 Stars: A very average hotel, I did enjoy the monarchy gazing',
+    reviews1:'exmaple review here',
+    reviews2:'exmaple review here',
     services: 'Sauna, Hypo-allergenic bedding, Free toiletries'
   },
 ];
